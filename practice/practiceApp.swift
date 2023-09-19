0c05741162b4168963ef99e65eba98e9f1bc4e51
Jacob Kotzian <kotzian49@gmail.com>
2023-09-19 19:27:47 -0400

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Jake Kotzian on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
